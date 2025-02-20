253001e6b87f37abfc9a251176195b12db9f1bcc
Samuel-Labagnere <samuel.labagnere@gmail.com>
2025-02-20 16:36:13 +0100

    Removed about page

--- apps/a11y-front/src/app/app.routes.ts
+++ apps/a11y-front/src/app/app.routes.ts
@@ -19,10 +19,6 @@ export const appRoutes: Route[] = [
     component: HomePageComponent
   },
   {
-    path: 'about',
-    loadChildren: () => import('./pages/about-page/about.routes').then(m => m.aboutRoutes)
-  },
-  {
     path: 'contact',
     component: ContactPageComponent,
   },
