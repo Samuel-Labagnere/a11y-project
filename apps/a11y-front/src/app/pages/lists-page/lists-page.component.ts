e0e90bfec038764f7c8a26630e08f71aaff87432
Samuel-Labagnere <71769022+Samuel-Labagnere@users.noreply.github.com>
2025-02-20 17:19:47 +0100

    Revert "Navbar"

--- apps/a11y-front/src/app/pages/lists-page/lists-page.component.ts
+++ apps/a11y-front/src/app/pages/lists-page/lists-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HeaderComponent} from "../../components/header/header.component";
 
 @Component({
   selector: 'a11y-lists-page',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule],
   templateUrl: './lists-page.component.html',
   styleUrls: ['./lists-page.component.scss'],
 })
