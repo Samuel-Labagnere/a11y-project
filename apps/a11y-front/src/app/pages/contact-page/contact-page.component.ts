1c5879400ea9f3f1823a5d8c86a41cfbc01a343d
Loïc Souêtre <loic.souetre@sfr.fr>
2025-03-20 12:16:41 +0100

    Add error system and messages to contact form

--- apps/a11y-front/src/app/pages/contact-page/contact-page.component.ts
+++ apps/a11y-front/src/app/pages/contact-page/contact-page.component.ts
@@ -1,11 +1,18 @@
-import {Component, inject} from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormsModule} from "@angular/forms";
-import {Router} from "@angular/router";
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
+import { NgForm } from '@angular/forms';
 
 export class ContactFormData {
-  constructor(public name: string, public age: number, public email: string, public phoneNumber: string, public message: string) {}
+  constructor(
+    public name: string,
+    public age: number,
+    public email: string,
+    public phoneNumber: string,
+    public message: string
+  ) {}
 }
 
 @Component({
@@ -19,9 +26,11 @@ export class ContactPageComponent {
   data = new ContactFormData('', 0, '', '', '');
   router = inject(Router);
 
-  onSubmit() {
-    alert('Thanks for your message!');
-    console.log('Sent with', JSON.stringify(this.data));
-    this.router.navigateByUrl('/');
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      alert('Thanks for your message!');
+      console.log('Sent with', JSON.stringify(this.data));
+      this.router.navigateByUrl('/');
+    }
   }
 }
