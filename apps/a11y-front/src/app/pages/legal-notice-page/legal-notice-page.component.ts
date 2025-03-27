f7d04b368a281cf9e75260dbcd51e36663c86537
Loïc Souêtre <loic.souetre@sfr.fr>
2025-03-27 10:36:48 +0100

    Add legal notice

--- apps/a11y-front/src/app/pages/legal-notice-page/legal-notice-page.component.ts
+++ apps/a11y-front/src/app/pages/legal-notice-page/legal-notice-page.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'a11y-legal-notice-page',
   standalone: true,
-  imports: [CommonModule, TranslateModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './legal-notice-page.component.html',
   styleUrls: ['./legal-notice-page.component.scss'],
 })
-export class AboutPageComponent {}
+export class LegalNoticePageComponent {}
