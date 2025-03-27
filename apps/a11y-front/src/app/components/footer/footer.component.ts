720f462125f90dd266097ea102bac4fd562887d2
Loïc Souêtre <loic.souetre@sfr.fr>
2025-03-27 11:30:21 +0100

    Add gcu page

--- apps/a11y-front/src/app/components/footer/footer.component.ts
+++ apps/a11y-front/src/app/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { NewsletterComponent } from '../newsletter/newsletter.component';
 
 @Component({
   selector: 'a11y-footer',
   standalone: true,
-  imports: [CommonModule, NewsletterComponent],
+  imports: [CommonModule, RouterLink, NewsletterComponent],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
