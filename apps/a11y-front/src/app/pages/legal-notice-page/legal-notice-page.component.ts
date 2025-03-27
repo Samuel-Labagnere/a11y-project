b27f1950ffbd1becba48d61a067da1b37a422d10
Samuel-Labagnere <samuel.labagnere@gmail.com>
2025-03-27 11:52:43 +0100

    Fixed mergure

--- apps/a11y-front/src/app/pages/legal-notice-page/legal-notice-page.component.ts
+++ apps/a11y-front/src/app/pages/legal-notice-page/legal-notice-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'a11y-legal-notice-page',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, FooterComponent, RouterLink],
   templateUrl: './legal-notice-page.component.html',
   styleUrls: ['./legal-notice-page.component.scss'],
 })
