a5003d47ea351df2e08b710d35364fb6dc00f1f7
Loïc Souêtre <loic.souetre@sfr.fr>
2025-03-27 10:22:01 +0100

    Add legal notice link in footer

--- apps/a11y-front/src/app/components/footer/footer.component.ts
+++ apps/a11y-front/src/app/components/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 import { NewsletterComponent } from '../newsletter/newsletter.component';
 
 @Component({
   selector: 'a11y-footer',
   standalone: true,
-  imports: [CommonModule, NewsletterComponent],
+  imports: [CommonModule, NewsletterComponent, RouterLink],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
